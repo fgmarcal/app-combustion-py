c334fe8d26dca207f64fcf65483c6fdd9171e7c8
fgmarcal <felipegarciamarcal@gmail.com>
2023-07-06 21:25:42 -0300

    tentativa de incluir outras linguages

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-import kivy
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-import random
-
-kivy.require('1.9.0')
-
-class MyRoot(BoxLayout):
-    def __init__(self):
-        super(MyRoot, self).__init__()
-    
-    def generate_number(self):
-        self.random_label.text = str(random.randint(0,100))
-    
-
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class Combust(App):
-    def build(self):
-        return MyRoot()
 
-combustion = Combust()
-combustion.run()
+    def build(self):
+
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.4, 0.3)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        return self.window
+
+if __name__ == "__main__":
+    Combust().run()
