ed46c5b2e30f8c730563d8d0c668f5d337186222
Felipe Garcia Marçal <90734397+fgmarcal@users.noreply.github.com>
2023-07-24 13:32:43 -0300

    fix outer scope value

--- final_version/engine.py
+++ final_version/engine.py
@@ -1,8 +1,15 @@
 import numpy as np
 
+def error():
+    raise ValueError("Value not found")
 
 def interpolate(x_axis, y_axis, value):
-    grau = 10
-    ajuste = np.polyfit(x_axis, y_axis, grau)
-    p = np.poly1d(ajuste)
-    return p(value)
+    if value >= x_axis[0] and value <= x_axis[-1]:
+        grau = 10
+        ajuste = np.polyfit(x_axis, y_axis, grau)
+        p = np.poly1d(ajuste)
+        return p(value)
+    else:
+        error() 
+
+#TODO
