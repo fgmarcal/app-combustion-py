3ee63196d5c1d7d0088bfbd99d7bf82b23c68bf5
fgmarcal <felipegarciamarcal@gmail.com>
2023-07-27 22:31:58 -0300

    delete comments

--- final_version/engine.py
+++ final_version/engine.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-# def interpolate(x_axis, y_axis, value):
-#     try:
-#         return np.interp(value, x_axis, y_axis)
-#     except Exception as err:
-#         print(err)
-#         return 0
-
 def interpolate(x_axis, y_axis, value):
     try:
         grau = 10
